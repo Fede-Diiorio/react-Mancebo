92209bd64c502c0e717d302c8b6c484a125aeb8b
Federico Di Iorio <federico_metallica@hotmail.com>
2024-02-09 19:24:42 -0300

    Migracion a vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
-import Home from './componets/Home/Home';
-import CartContent from "./componets/CartContent/CartContent";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from "react"
+
+import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+import NavBar from "./components/NavBar/NavBar";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 function App() {
+  const [cart, setCart] = useState([])
+  console.log(cart)
+
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/cart" element={<CartContent />} />
-    </Routes>
-    </BrowserRouter>
-    
-  );
+    <>
+      <BrowserRouter>
+        <NavBar cart={cart}/>
+        <Routes> 
+         <Route path='/' element={<ItemListContainer greeting={'Listado de Comics'}/>} />
+         <Route path='/category/:categoryId' element={<ItemListContainer greeting={'Listado de Comics filtrados'}/>} />
+         <Route path='/detail/:productId' element={<ItemDetailContainer setCart={setCart} />} />        
+        </Routes>
+      </BrowserRouter>  
+    </>
+  )
 }
 
-export default App;
+export default App
