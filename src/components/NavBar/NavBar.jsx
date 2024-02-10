8b14dc7ad302216c7be00bc48eb7eada90586d80
Federico Di Iorio <federico_metallica@hotmail.com>
2024-02-10 19:06:16 -0300

    Ahora se oculta el icono de carrito

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,16 +1,19 @@
 import { Link } from "react-router-dom";
 import CartWidget from "../CartWidget/CartWidget";
 import styles from './NavBar.module.css';
+import { useCart } from "../../context/CartContext";
+
 const NavBar = () => {
+    const { totalQuantity } = useCart()
     return (
         <nav className={styles.navbar}>
             <div className={styles.navbarContainer}>
                 <Link to={'/'} className={styles.link}><h1>ComicShop</h1></Link>
+                {totalQuantity > 0 ? <CartWidget /> : null}
                 <section className={styles.navLinks}>
                     <Link to={'/category/Dc Comics'}>Dc Comics</Link>
                     <Link to={'/category/Marvel Comics'}>Marvel Comics</Link>
                 </section>
-                <CartWidget />
             </div>
         </nav>
     );
